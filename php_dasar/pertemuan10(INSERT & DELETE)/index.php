f5bc3e19afcfd8a833836a150d85d87d6e00c88b
Azhandi Usemahu <55913058+nulla-vis@users.noreply.github.com>
2021-03-18 11:35:55 +0700

    pagination
    add pagination function

--- php_dasar/pertemuan10(INSERT & DELETE)/index.php
+++ php_dasar/pertemuan10(INSERT & DELETE)/index.php
@@ -8,13 +8,32 @@ if(!isset($_SESSION["login"])) {
 
 require 'functions.php';
 
-//query data dari database--------------------------------------------------------------------------------------------------------
-$mahasiswa = query("SELECT * FROM mahasiswa");
+//query data dari database=======================================================================================================
+//pagination---------------------------------------------------------------------------------------------------------------------
+if( empty($_SESSION["keyword"]) ) {
+    $mahasiswa = pagination()["query"];
+    $jumlahHalaman = pagination()["jumlahHalaman"];
+    $halamanAktif = pagination()["halamanAktif"];
+    $awalData = pagination()["awalData"];
+} else {
+    $hasil = cari($_SESSION["keyword"]);
+    $mahasiswa = $hasil["query"];
+    $jumlahHalaman = $hasil["jumlahHalaman"];
+    $hasil2 = cari($_SESSION["keyword"]);
+    $halamanAktif = $hasil2["halamanAktif"];
+    $awalData = $hasil2["awalData"];
+}
+
+
 // var_dump($mahasiswa[0]);
 
-//jika tombol cari diclick, data di variable $mahasiswa akan ditimpa sesuai keyword pencatian-------------------------------------
+//jika tombol cari diclick, data di variable $mahasiswa akan ditimpa sesuai keyword pencarian-------------------------------------
 if( isset($_POST["cari"]) ) {
-    $mahasiswa = cari($_POST["keyword"]);
+    $mahasiswa = cari($_POST["keyword"])["query"];
+    $jumlahHalaman = cari($_POST["keyword"])["jumlahHalaman"];
+    $_SESSION["keyword"] = $_POST["keyword"];
+    $halamanAktif = cari($_SESSION["keyword"])["halamanAktif"];
+    header("Location: ?halaman=1");
 }
 
 ?>
@@ -39,6 +58,25 @@ if( isset($_POST["cari"]) ) {
     <input type="text" name="keyword" size="40" autofocus placeholder="input search keyword" autocomplete="off">
     <button type="submit" name="cari">Cari</button>
     <br><br>
+    <!-- jika ada data yang bisa ditampilkan=================================== -->
+    <?php if(count($mahasiswa) > 0) : ?>
+    <!-- navigasi pagination ===================================================-->
+    <?php if($halamanAktif > 1) : ?>
+        <a href="?halaman=<?= $halamanAktif-1; ?>">&laquo;</a>
+    <?php endif; ?>
+
+    <?php for($i=1; $i <= $jumlahHalaman; $i++) : ?>
+        <?php if($halamanAktif == $i) :?>
+            <a href="?halaman=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?></a>
+        <?php else :?>
+            <a href="?halaman=<?= $i; ?>"><?= $i; ?></a>
+        <?php endif; ?>
+    <?php endfor; ?>
+
+    <?php if($halamanAktif < $jumlahHalaman) : ?>
+        <a href="?halaman=<?= $halamanAktif+1; ?>">&raquo;</a>
+    <?php endif; ?>
+    <!-- ======================================================================= -->
 </form>
     <table border="1" cellpadding="10" cellspacing="0">
         <tr>
@@ -53,7 +91,7 @@ if( isset($_POST["cari"]) ) {
         <?php $i = 1; ?>
         <?php foreach ($mahasiswa as $row) :?>
         <tr>
-            <td><?= $i; ?></td>
+            <td><?= $i + $awalData; ?></td>
             <td>
                 <a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> |
                 <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Yakin?')">hapus</a>
@@ -67,6 +105,11 @@ if( isset($_POST["cari"]) ) {
         <?php $i++; ?>
         <?php endforeach ?>
     </table>
+
+    <?php else : ?>
+    <!-- jika tidak ada data yang bisa ditampilkan============================= -->
+        <p style="color: red; font-style: italic;">data tidak ada / tidak ditemukan</p>
+    <?php endif; ?>
 
 
 </body>

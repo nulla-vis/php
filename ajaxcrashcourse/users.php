4fcbda426804279b0ec8e2027fe75f6795bd3d8c
Azhandi Usemahu <55913058+nulla-vis@users.noreply.github.com>
2021-03-18 11:54:49 +0700

    Add files via upload

---
+++ ajaxcrashcourse/users.php
@@ -0,0 +1,15 @@
+<?php 
+//connect to database
+
+$conn = mysqli_connect('localhost','root','','ajaxtest');
+
+$query = 'SELECT * FROM users';
+
+// Get Result
+$result = mysqli_query($conn, $query);
+
+//Fetch Data
+$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo json_encode($users);
+?>
